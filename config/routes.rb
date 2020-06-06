a45869ea3cf8747555b598f51623f1dd3a27b3c2
novakoda <novakoda9@gmail.com>
2020-06-06 01:46:38 -0500

    added names to users and fixed devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
 
   root 'posts#index', as: 'home'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_06_06_013557) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
