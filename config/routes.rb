bc1ff078b505491abcd02c029a122d31314cfdcc
novakoda <novakoda9@gmail.com>
2020-06-07 00:19:18 -0500

    Created user controller/Can now view user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'user/:id', to: 'user#show', as: 'user'
+  get 'users', to: 'user#index', as: 'user_index'
   resources :posts
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, controllers: {
         registrations: 'users/registrations'
@@ -7,4 +9,5 @@ Rails.application.routes.draw do
   root 'posts#index', as: 'home'
 
   post 'users', to: 'devise/registrations#create', as: 'new_user'
+
 end
