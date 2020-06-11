0e0666d23d223c323de32236adecdd49d6cccd66
JR Nova <novakoda9@gmail.com>
2020-06-11 01:05:35 -0500

    fixed user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/:id', to: 'users#show', as: 'user'
-  get 'users', to: 'users#index', as: 'user_index'
-
   resources :posts
 
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, controllers: {
@@ -13,5 +10,8 @@ Rails.application.routes.draw do
   post 'users', to: 'devise/registrations#create', as: 'new_user'
 
   get 'users/:requestee_id/friend_request', to: 'users#friend_request', as: 'request'
+
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users', to: 'users#index', as: 'user_index'
 
 end
