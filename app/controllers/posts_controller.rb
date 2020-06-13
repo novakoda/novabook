abfc4f4fd1aa69b7bca1c728f34f30e2eac9be33
JR Nova <novakoda9@gmail.com>
2020-06-13 17:28:29 -0500

    user needs to be logged in to see pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,11 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /posts
   # GET /posts.json
   def index
-    if user_signed_in?
-      @posts = Post.where(id: current_user.friends.ids)
-      @post = current_user.posts.new
-    else
-      redirect_to new_user_session_path
-    end
+    @posts = Post.where(id: current_user.friends.ids)
+    @post = current_user.posts.new
   end
 
   # GET /posts/1
